5ecc863bc6b0c6fbbd6b2d5aa94e195912d813cd
oluoko <brianotieno586@gmail.com>
2024-11-09 10:56:51 +0300

    Openai Agent: Tools: generateImage, dadJoke, reddit(chess page)

---
+++ src/tools/generateImage.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod'
+import type { ToolFn } from '../../types'
+import { openai } from '../ai'
+
+export const generateImageToolDefinition = {
+  name: 'generate_image',
+  parameters: z.object({
+    prompt: z
+      .string()
+      .describe(
+        `prompt for the image. be sure to consider the  user's original message when making the prompt. if you are unsure, then ask the user  to provide more details`
+      ),
+  }),
+  description: 'generate an image based on a prompt',
+}
+
+type Args = z.infer<typeof generateImageToolDefinition.parameters>
+
+export const generateImage: ToolFn<Args, string> = async ({
+  toolArgs,
+  userMessage,
+}) => {
+  const response = await openai.images.generate({
+    model: 'dall-e-3',
+    n: 1,
+    prompt: toolArgs.prompt,
+    size: '1024x1024',
+  })
+
+  return response.data[0].url
+}
---
+++ src/tools/index.ts
@@ -0,0 +1,5 @@
+import { generateImage } from './generateImage'
+import { dadJoke } from './dadJoke'
+import { reddit } from './reddit'
+
+export const tools = [generateImage, dadJoke, reddit]
